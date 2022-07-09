908795edae48e0d8d679a542e4c5557ebeab9f90
Lord <github@gor1lla.de>
2022-07-09 17:08:46 +0200

    save card language and mail between stalks

--- src/main/java/de/gor1lla/cardstalker/web/WebController.java
+++ src/main/java/de/gor1lla/cardstalker/web/WebController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -26,15 +27,18 @@ public class WebController {
     }
 
     @GetMapping
-    public String getCardForm(Model model) {
+    public String getCardForm(Model model, HttpSession session) {
 
         model.addAttribute("card", new Card());
+        model.addAttribute("keepLocale", session.getAttribute("keepLocale"));
+        model.addAttribute("keepMail", session.getAttribute("keepMail"));
         return "form";
     }
 
     @PostMapping
-    public String addCard(@ModelAttribute Card card) {
-
+    public String addCard(@ModelAttribute Card card, HttpSession session) {
+        session.setAttribute("keepLocale", card.getLocale());
+        session.setAttribute("keepMail", card.getMailAddress());
         cardManagementService.saveCard(card);
         return "redirect:/";
     }
