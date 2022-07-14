743c68ff78d071a7fd446a5ca98da576294aec29
Lord <github@gor1lla.de>
2022-07-14 11:52:49 +0200

    check for correct host

--- src/main/java/de/gor1lla/cardstalker/web/WebController.java
+++ src/main/java/de/gor1lla/cardstalker/web/WebController.java
@@ -1,11 +1,12 @@
 package de.gor1lla.cardstalker.web;
 
 import de.gor1lla.cardstalker.domain.Card;
-import de.gor1lla.cardstalker.repositories.CardRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,9 +37,16 @@ public class WebController {
     }
 
     @PostMapping
-    public String addCard(@ModelAttribute Card card, HttpSession session) {
+    public String addCard(@ModelAttribute Card card, HttpSession session, BindingResult result) {
+
         session.setAttribute("keepLocale", card.getLocale());
         session.setAttribute("keepMail", card.getMailAddress());
+
+        if (!card.getUrl().matches("^http(s)?\\:\\/\\/(www)?\\.cardmarket\\.(net|com)\\/.*")) {
+            FieldError error = new FieldError("card", "url", "Not a cardmarket link");
+            result.addError(error);
+            return "form";
+        }
         cardManagementService.saveCard(card);
         return "redirect:/";
     }
